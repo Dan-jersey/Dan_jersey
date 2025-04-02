5da166efb6e74034f39a60a11ffa78ce65028bdf
Dan-jersey <stromjersey@gmail.com>
2025-04-03 00:09:33 +0100

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -14,7 +14,7 @@ module.exports = {
   },
 
   onStart: async function ({ message, args, api, event }) {
-    const permission = ["61564382117276"];
+    const permission = ["61567813072795"];
     if (!permission.includes(event.senderID)) {
       return api.sendMessage("𝐓𝐔 𝐍'𝐞𝐬 𝐩𝐚𝐬 𝐚𝐝𝐦𝐢𝐧𝐢𝐬𝐭𝐫𝐚𝐭𝐞𝐮𝐫 𝐝𝐞 𝐜𝐞 𝐛𝐨𝐭 ' ", event.threadID, event.messageID);
     }
